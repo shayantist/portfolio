10e9073c5c8680246b1daed15968b0d8fea9d71c
Shayan Chowdhury <shayan@reach4help.org>
2021-11-20 11:51:39 -0500

    feat: testing new python serverless api

---
+++ src/pages/play.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import {
+  HeroSection,
+  HeroTextWrapper,
+  HeroFunWrapper,
+  WorksSection,
+  WorkCard,
+} from "../styles/pages/index.styles";
+
+import { Page, Time } from "../components";
+import parseMarkdown from "../utils/parseMarkdown";
+
+function PlayPage() {
+  return (
+    <Page>
+      <Head>
+        <title>Shayan Chowdhury | Playground</title>
+      </Head>
+      <HeroSection>
+        <HeroTextWrapper>
+          <h1>Project Playground</h1>
+        </HeroTextWrapper>
+      </HeroSection>
+
+      <WorksSection>
+        <p>
+          <Time />
+        </p>
+        <p>If time above is showing, then our python server is running!</p>
+      </WorksSection>
+    </Page>
+  );
+}
+
+export default PlayPage;

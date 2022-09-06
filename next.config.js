46de5ffa9f74aa31b54424ceb769a0366da26bdb
Shayan Chowdhury <shayan@reach4help.org>
2022-09-06 16:55:52 -0400

    feat: add route for resume (attempt 2)

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ module.exports = {
     domains: ["s3.us-west-2.amazonaws.com"],
     minimumCacheTTL: 31536000,
   },
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination: "/docs/Shayan%20Chowdhury%20-%20Resume.pdf",
+        permanent: false,
+      },
+    ];
+  },
 };

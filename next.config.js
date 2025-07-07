d999f1cb222a081a320df24c0f3aae4312bfca8a
Shayan Chowdhury <shayan@reach4help.org>
2025-07-07 17:18:32 -0400

    added API path setup

--- next.config.js
+++ next.config.js
@@ -16,4 +16,15 @@ module.exports = {
   compiler: {
     emotion: true,
   },
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:3000/api/:path*"
+            : "/api/",
+      },
+    ];
+  },
 };
